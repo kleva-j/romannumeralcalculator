7ab2f136d100bedfa9cea21fd765a03858b7fb12
kleva-j <kasmickleva@gmail.com>
2019-10-03 01:33:23 +0100

    implement functionality to convert a sumed array of input to roman numerals

--- src/RomanNumeralComponent.js
+++ src/RomanNumeralComponent.js
@@ -1,18 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
+export const romanNumeralsMapping = {
+    'M' : 1000,
+    'CM': 900,
+    'D' : 500,
+    'CD': 400,
+    'C' : 100,
+    'XC': 90,
+    'L' : 50,
+    'XL': 40,
+    'X' : 10,
+    'IX':  9,
+    'V' :  5,
+    'IV':  4,
+    'I' :  1,
+};
 
 export default function RomanNumeralComponent () {
     const [calculatedAnswer, updateAnswer] = useState("Nulla");
     const [input, updateInput] = useState("");
 
-
     const addAndConvertToRomanNumerals = (ints) => {
-        /* Implement me! */
-        return ints;
+        let sumOfInputValues = ints
+            .reduce((total, currentItem) => (total + currentItem), 0);
+            
+        let romanNumeral = '';
+
+        for (let key in romanNumeralsMapping) {
+            while(sumOfInputValues >= romanNumeralsMapping[key]) {
+                romanNumeral += key;
+                sumOfInputValues -= romanNumeralsMapping[key];
+            }
+        }
+
+        return romanNumeral || 'Nulla';
     }
 
     const addNumbers = (inputString) => {
         const numbersStringArray = inputString.split(",");
-        const numbers = numbersStringArray.map((numberAsString) => { return parseInt(numberAsString, 10) })
+        const numbers = numbersStringArray
+            .map((numberAsString) => parseInt(numberAsString, 10));
 
         /* numbers is an array of ints. E.g. [1, 2, 3] */
         const answer = addAndConvertToRomanNumerals(numbers)
